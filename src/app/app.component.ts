1e214ad2a9eaac2267f548ae5e3c0a483afd155a
MarilynWaldman <mdwaldman22@hotmail.com>
2018-04-19 10:25:38 -0600

    working with materail and tree structure - doesn't look very pretty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
+  //template: '<tree-root [nodes]="nodes" [options]="options"></tree-root>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  {
+  
+
+   nodes = [
+    {
+      id: 1,
+      name: 'Prague',
+      icon: 'place',
+      children: [
+        { id: 2, name: 'Cohen', icon: 'person' },
+        { id: 3, name: 'Steele', icon: 'person' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'Russian Connections',
+      icon: 'add_box',
+      children: [
+        { id: 5, name: 'Bannon',
+          icon: 'add_box',
+          children: [
+            { id: 6, name: 'Donald Trump Jr', icon: 'add_box',
+             children : [
+              { id: 8, name: 'Donald Trump Jr', icon: 'person' },
+              { id: 9, name: 'Jared Kushner', icon: 'person' }             
+             ] },
+            { id: 7, name: 'Jared Kushner', icon: 'person' }
+          ]             
+        },
+        { id: 8, name: 'Donald Trump Jr', icon: 'person' },
+        { id: 9, name: 'Jared Kushner', icon: 'person' },
+        { id: 10, name: 'Michael T. Flynn', icon: 'person' },
+        { id: 11, name: 'Russian ambassador, Sergey I. Kislyaks', icon: 'person' },
+        { id: 12, name: 'Paul Manafort', icon: 'person' },
+        { id: 13, name: 'Carter Page', icon: 'person' },
+        { id: 14, name: 'Rodge Stone', icon: 'person' },
+        { id: 15, name: 'Guccifer 2.0', icon: 'person' }
+      ]
+    }
+  ];
+  options = {};
+
+  f1() : void{
+    console.log("hi");
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TreeModule } from 'angular-tree-component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DomSanitizer} from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
+import { MatIconRegistry} from '@angular/material';
 
 
 @NgModule({
@@ -10,9 +14,19 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    TreeModule
   ],
+  exports: [
+    AppComponent,
+    MaterialModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+//export class AppModule { }
+export class AppModule {}
+
+  
